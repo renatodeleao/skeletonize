ef8a2aee7f14eea672d4d181f10fa1450e852bde
Renato de Leão <renatodeleao@gmail.com>
2018-11-01 21:59:42 +0000

    https://codepen.io/ohsimtabem/pen/dQbyeG
    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+/**
+* Skeletonize.js
+* Dynamic skeleton loaders with minimal effort
+*/
+class Skeletonize {
+  constructor(node, options) {
+    this.node = node;
+    this.skeletonEls = [];
+
+    this.init();
+  }
+
+  init = () => {
+    this._walkDOM(this.node);
+  }
+
+  add = () => {
+    this.skeletonEls.map(el => {
+      el.style.position = "relative";
+      el.classList.add('skeletonizejs');
+    })
+  }
+
+  remove = () => {
+    this.skeletonEls.map(el => {
+      el.style.position = "";
+      el.classList.remove('skeletonizejs');
+    })
+  }
+
+  /**
+  * @private
+  * walkDom
+  * Recursive get and store nodes
+  * Adapted from Javascript: the good parts
+  */
+  _walkDOM = (node) => {
+    this._filter(node);
+    node = node.firstElementChild;
+    while(node) {
+      this._walkDOM(node, this._filter);
+      node = node.nextElementSibling;
+    }
+  };
+
+  _filter = (node) => {
+    if(node.children.length === 0){
+      this.skeletonEls.push(node)
+    }
+  }
+}
